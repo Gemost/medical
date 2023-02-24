1307bbb993cdc915f7c0ffc6c0b26e57b5d286ee
郭映杉 <1305539882@qq.com>
2023-02-24 16:13:22 +0800

    添加了city属性

---
+++ src/main/java/com/orthopaedicsystem/po/City.java
@@ -0,0 +1,15 @@
+package com.orthopaedicsystem.po;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+
+public class City {
+    Integer city_id;
+
+    Date creattime;
+    Date updatetime;
+    Integer city_numb;
+
+}
