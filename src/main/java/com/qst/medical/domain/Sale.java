41f0e5dcae02a902957be086147a6576e152b7fc
u15868372516 <vinkle-hzt@outlook.com>
2023-03-06 11:34:27 +0800

    添加销售地点经纬度

--- src/main/java/com/qst/medical/domain/Sale.java
+++ src/main/java/com/qst/medical/domain/Sale.java
@@ -15,6 +15,8 @@ public class Sale extends SuperDomain {
     private String salePhone;//药店电话
     private Date createtime;//创建时间
     private Date updatetime;//修改时间
+    private Double lng;
+    private Double lat;
     //省略get/set方法
 
 }
